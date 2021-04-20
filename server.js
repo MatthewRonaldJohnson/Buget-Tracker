fd07cdd3980e761e50cc97632e48c78998b3f365
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-04-19 21:09:15 -0500

    added process.env.PORT to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 7999;
+const PORT = process.env.PORT || 7999;
 
 const app = express();
 
